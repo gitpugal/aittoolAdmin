6e301353188903e0d4f88a03da9077c8116765df
unknown <pugalarasan2014@gmail.com>
2023-09-03 00:17:48 +0530

    shadcn initial commit

--- app/api/db.ts
+++
@@ -1,9 +0,0 @@
-import pgPromise from 'pg-promise';
-
-const pgp = pgPromise();
-
-const connectionString: string =
-  'postgres://sourcefreeze:k2NuUA4SsFMb@ep-divine-sound-318147-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require';
-const db: pgPromise.IDatabase<{}> = pgp(connectionString);
-
-export default db;
